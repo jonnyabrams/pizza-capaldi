796f0b8b7f6468af7cd3027e87d07938714b7b99
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-12 19:57:59 -0400

    feat: individual Product page with size chooser

---
+++ pages/product/[id].tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import { useState } from "react";
+
+import styles from "../../styles/Product.module.css";
+
+const Product = () => {
+  const [size, setSize] = useState(0);
+
+  const pizza = {
+    id: 1,
+    img: "/img/pizza.png",
+    name: "PIZZA A LA TOMATO",
+    price: [12.99, 13.99, 14.99],
+    desc: "It's got tomato on it, and maybe some other stuff",
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles.imgContainer}>
+          <Image src={pizza.img} layout="fill" alt="" objectFit="contain" />
+        </div>
+      </div>
+      <div className={styles.right}>
+        <h1 className={styles.title}>{pizza.name}</h1>
+        <span className={styles.price}>£{pizza.price[size].toFixed(2)}</span>
+        <p className={styles.desc}>{pizza.desc}</p>
+        <h3 className={styles.choose}>Choose size:</h3>
+        <div className={styles.sizes}>
+          <div className={styles.size} onClick={() => setSize(0)}>
+            <Image src="/img/size.png" layout="fill" alt="" />
+            <span className={styles.number}>Small</span>
+          </div>
+          <div className={styles.size} onClick={() => setSize(1)}>
+            <Image src="/img/size.png" layout="fill" alt="" />
+            <span className={styles.number}>Medium</span>
+          </div>
+          <div className={styles.size} onClick={() => setSize(2)}>
+            <Image src="/img/size.png" layout="fill" alt="" />
+            <span className={styles.number}>Large</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
