2c8c7fd7e3e1e3373e0491aec4fde8b0e2718260
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-13 15:09:31 -0400

    feat: make Cart responsive

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -8,7 +8,7 @@ const Cart = () => {
       <div className={styles.left}>
         <table className={styles.table}>
           <tbody>
-            <tr className={styles.tr}>
+            <tr className={styles.trTitle}>
               <th>Product</th>
               <th>Name</th>
               <th>Extras</th>
@@ -16,7 +16,7 @@ const Cart = () => {
               <th>Quantity</th>
               <th>Total</th>
             </tr>
-            <tr>
+            <tr className={styles.tr}>
               <td>
                 <div className={styles.imgContainer}>
                   <Image
