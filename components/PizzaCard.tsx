f1e9abcd27060904050121078d2e459814788230
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-16 12:10:59 -0400

    feat: click through to individual product page

--- components/PizzaCard.tsx
+++ components/PizzaCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import styles from "../styles/PizzaCard.module.css";
 import { IPizza } from "../types";
@@ -10,7 +11,9 @@ interface IProps {
 const PizzaCard = ({ pizza }: IProps) => {
   return (
     <div className={styles.container}>
+      <Link href={`/product/${pizza._id}`}>
       <Image src={pizza.img} alt="" height="500" width="500" />
+      </Link>
       <h1 className={styles.title}>{pizza.title}</h1>
       <span className={styles.price}>${pizza.prices[0]}</span>
       <p className={styles.desc}>{pizza.desc}</p>
