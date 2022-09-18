d3075acd4892c0fcb4bbb0973e481c703dfc1d6b
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-17 20:50:15 -0400

    fix: display correct totals on order conf page

--- pages/order/[id].tsx
+++ pages/order/[id].tsx
@@ -102,13 +102,13 @@ const Order = ({ order }: { order: IOrder }) => {
         <div className={styles.wrapper}>
           <h2 className={styles.title}>CART TOTAL</h2>
           <div className={styles.totalText}>
-            <b className={styles.totalTextTitle}>Subtotal:</b> £12.99
+            <b className={styles.totalTextTitle}>Subtotal:</b> £{order.total}
           </div>
           <div className={styles.totalText}>
             <b className={styles.totalTextTitle}>Discount:</b>£0.00
           </div>
           <div className={styles.totalText}>
-            <b className={styles.totalTextTitle}>Total:</b>£12.99
+            <b className={styles.totalTextTitle}>Total:</b>£{order.total}
           </div>
           <button disabled className={styles.button}>
             PAID
