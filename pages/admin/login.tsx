c4403a1d1bdc1a7d4afe42d7d1b6b1261f47c7aa
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-20 14:36:50 +0100

    feat: frontend admin login

--- pages/admin/login.tsx
+++ pages/admin/login.tsx
@@ -1,9 +1,49 @@
-import styles from "../styles/Login.module.css";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+import styles from "../../styles/Login.module.css";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const router = useRouter();
 
-export default Login
+  const handleClick = async () => {
+    try {
+      await axios.post("http://localhost:3000/api/login", {
+        username,
+        password,
+      });
+      router.push("/admin");
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1>Admin Dashboard</h1>
+        <input
+          placeholder="username"
+          className={styles.input}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          placeholder="password"
+          type="password"
+          className={styles.input}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleClick} className={styles.button}>
+          Sign In
+        </button>
+        {error && <span className={styles.error}>Wrong credentials!</span>}
+      </div>
+    </div>
+  );
+};
+
+export default Login;
